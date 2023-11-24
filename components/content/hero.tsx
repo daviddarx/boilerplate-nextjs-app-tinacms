eeb9da019a6fa7b0d9c266daa9f29039f97cbd09
David Darx <david.darx@liip.ch>
2023-11-24 20:30:26 +0100

    feat(tina): add tina fields edit shortcuts

--- components/content/hero.tsx
+++ components/content/hero.tsx
@@ -1,12 +1,17 @@
 import { CustomMarkdown } from '@/components/ui/custom-markdown';
 import { PageBlocksHero } from '@/tina/__generated__/types';
 import classNames from 'classnames';
+import { tinaField } from 'tinacms/dist/react';
 
 export function Hero(props: PageBlocksHero) {
   return (
     <section>
-      <h2 className='font-bold'>{props.title}</h2>
-      <CustomMarkdown content={props.description} />
+      <h2 className='font-bold' data-tina-field={tinaField(props, 'title')}>
+        {props.title}
+      </h2>
+      <div data-tina-field={tinaField(props, 'description')}>
+        <CustomMarkdown content={props.description} />
+      </div>
       <div className='flex items-start gap-12'>
         {props.links?.map((link, i) => (
           <a
@@ -15,6 +20,7 @@ export function Hero(props: PageBlocksHero) {
             className={classNames('inline-block px-12 py-2 rounded-full border border-gray-200', {
               'bg-black border-black text-white': link?.style === 'primary',
             })}
+            data-tina-field={tinaField(link!, 'label')}
           >
             {link?.label}
           </a>
