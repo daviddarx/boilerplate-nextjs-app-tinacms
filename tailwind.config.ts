0c44de4bed26288cdbdec69bdf981afa2f7e1a9f
David Darx <david.darx@liip.ch>
2023-11-24 15:07:50 +0100

    feat(setup): streamline

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    spacing: {
+      0: '0', // 0
+      1: '1px', // 1px
+      2: '0.125rem', // 2px
+      3: '0.1875rem', // 3px
+      4: '0.25rem', // 4px
+      6: '0.3125rem', // 6px
+      8: '0.5rem', // 8px
+      10: '0.625rem', // 10px
+      12: '0.75rem', // 12px
+      14: '0.875rem', // 14px
+      16: '1rem', // 16px
+      18: '1.125rem', // 18px
+      20: '1.25rem', // 20px
+      24: '1.5rem', // 24px
+      28: '1.75rem', // 28px
+      32: '2rem', // 32px
+      40: '2.5rem', // 40px
+      48: '3rem', // 48px
+      56: '3.5rem', // 56px
+      64: '4rem', // 64px
+      72: '4.5rem', // 72px
+      80: '5rem', // 80px
+      96: '6rem', // 96px
+      120: '7.5rem', // 120px
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
