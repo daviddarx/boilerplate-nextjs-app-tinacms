2aac31677781eb8480b362cf36e68215c1ce83d1
David Darx <david.darx@liip.ch>
2024-01-19 12:39:36 +0100

    style(feature-list): update layout

--- components/content/feature-list.tsx
+++ components/content/feature-list.tsx
@@ -5,7 +5,7 @@ import { tinaField } from 'tinacms/dist/react';
 
 export default function FeatureList(props: PageBlocksFeatureList) {
   return (
-    <section className='border-t border-black pt-gutter lg:pr-gutter'>
+    <section className='border-t border-black pt-gutter'>
       <div className='text-container'>
         <h2 data-tina-field={tinaField(props, 'title')}>{props.title}</h2>
         <div data-tina-field={tinaField(props, 'description')}>
@@ -13,13 +13,13 @@ export default function FeatureList(props: PageBlocksFeatureList) {
         </div>
       </div>
 
-      <ul className='mt-gutter grid grid-cols-2 gap-12'>
+      <ul className='mt-gutter grid grid-cols-2 gap-2'>
         {props.feature?.map((feature, i) => (
           <li key={i} data-tina-field={tinaField(feature!, 'title')}>
             {feature?.image && (
               <Image src={feature.image} alt={feature.title} width={1920} height={1080} />
             )}
-            <div className='text-container mt-spacer-24'>
+            <div className='text-container mt-spacer-24 pr-gutter'>
               <h3>{feature?.title}</h3>
               <p>{feature?.description}</p>
             </div>
