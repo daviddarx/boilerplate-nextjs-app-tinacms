c96ac1fff7559b59492a28d354e0a1127a178b2a
David Darx <david.darx@liip.ch>
2024-01-19 13:02:58 +0100

    feat(custom-markdown): add className prop.

--- components/ui/custom-markdown.tsx
+++ components/ui/custom-markdown.tsx
@@ -1,11 +1,13 @@
+import classNames from 'classnames';
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
 import { TinaMarkdownContent } from 'tinacms/dist/rich-text';
 
 export default function CustomMarkdown(props: {
   content: TinaMarkdownContent | TinaMarkdownContent[];
+  className?: string;
 }) {
   return (
-    <div className='text-container'>
+    <div className={classNames('text-container', props.className)}>
       <TinaMarkdown
         content={props.content}
         components={{
