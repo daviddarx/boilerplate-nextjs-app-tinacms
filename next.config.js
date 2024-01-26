7fe602d18334dedb446a3fca2d7c54a43735acfb
David Darx <david.darx@liip.ch>
2024-01-26 13:17:00 +0100

    feat(tina): link to tina cloud

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   async rewrites() {
     return [{ source: '/admin', destination: '/admin/index.html' }];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.tina.io',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- tina/config.ts
+++ tina/config.ts
@@ -10,9 +10,9 @@ const slugify = (value = 'no-value') => {
 };
 
 export default defineConfig({
-  branch: '',
-  clientId: '',
-  token: '',
+  branch: 'main',
+  clientId: '5da5b003-4338-4ec2-acc1-a268bb829e8f',
+  token: 'a32794db148d85b99347084b0d23f0014c8bf9ba',
   build: {
     publicFolder: 'public',
     outputFolder: 'admin',
