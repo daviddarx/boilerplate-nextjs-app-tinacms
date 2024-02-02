6647ed95972521441e57874b306f433124e694e7
David Darx <david.darx@liip.ch>
2024-02-02 17:30:13 +0100

    feat(setup): complete data revalidation (incremental static generation)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import PageComponent from '@/components/app/page';
 import client from '@/tina/__generated__/client';
 
+export const revalidate = 10;
+
 export default async function Page() {
   const result = await client.queries.pageAndNav({ relativePath: 'home.mdx' });
 
