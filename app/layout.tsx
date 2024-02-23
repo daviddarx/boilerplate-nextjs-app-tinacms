c590067d7ee0fd38603a2eabbb4e0fa4d8c3add7
David Darx <david.darx@liip.ch>
2024-02-23 08:00:12 +0100

    feat(layout): centralize header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css';
+import Header from '@/components/layout/header';
 import classNames from 'classnames';
 import type { Metadata } from 'next';
 import { Bricolage_Grotesque } from 'next/font/google';
-import Link from 'next/link';
 
 const font = Bricolage_Grotesque({ weight: ['400', '600'], subsets: ['latin'] });
 
@@ -16,11 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang='en'>
       <body className={classNames('h-full w-full', font.className)}>
         <div className='lg:grid lg:grid-cols-2'>
-          <header>
-            <h2 className='p-gutter lg:fixed lg:left-0 lg:top-0'>
-              <Link href='/'>Logo</Link>
-            </h2>
-          </header>
+          <Header />
           <main className='p-gutter lg:px-0'>{children}</main>
         </div>
       </body>
---
+++ components/layout/header.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Logo() {
+  return (
+    <header>
+      <h2 className='h1 p-gutter lg:fixed lg:left-0 lg:top-0'>
+        <Link href='/'>Logo</Link>
+      </h2>
+    </header>
+  );
+}
